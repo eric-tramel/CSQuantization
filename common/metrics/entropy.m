0683eab799f0163f5c6e5283eeb03c3143b81050
Eric W. Tramel <eric.tramel@gmail.com>
2013-02-12 14:33:17 -0600

    Revert "Test commit"
    This reverts commit 472a0a5377a3cd99e7242115b2dca3b62bdadd80.

--- common/metrics/entropy.m
+++ common/metrics/entropy.m
@@ -3,7 +3,6 @@
 % assumes that the list of terms/values is limited to some dictionary of
 % integer vales. Using any kind of fractional values will, more than
 % likely, throw the entire calculation off.
-% Sungkwang touched this file.
 
 % Lets determine how many values we have in x
 c = unique(x);
