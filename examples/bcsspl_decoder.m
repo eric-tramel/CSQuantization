8ede6af1765ab82d133163834b0f1843059b52c2
Sungwkang Mun <munsungkwang@gmail.com>
2013-03-05 18:03:49 -0600

    Finishing bcsspl-dpcm on the csq structure

--- examples/bcsspl_decoder.m
+++ examples/bcsspl_decoder.m
@@ -20,31 +20,24 @@ Psi_t = params.invpsi;
 A_t = params.ATrans;
 
 TOL = params.tol; %0.0001;
-x = Phi_t(y);
+% x = Phi_t(y);
 
+x = A_t(y);
 % [af, sf] = farras;
 
 % params.end_levels = params.num_levels-1;
 D_prev = 0;
 for i = 1:max_iterations
   
-    x_hat = params.smoothing(x);
-% x = col2im(x, params.block_dim , params.imsize, 'distinct'); 
-x = reshape(x, params.imsize);
-x_hat = wiener2(x, [3, 3]);
-% x_hat = vectorize(im2col(x_hat, params.block_dim, 'distinct'));
-% x_hat = vectorize(x_hat);
-x_hat = x_hat(:);
-    
+    x_hat = Psi(params.smoothing(Psi_t(x)));
+%     x_hat = x_hat + Phi_t((y - Phi(x_hat)));
+    x_hat = x_hat + A_t(y - A(x_hat));
+%     x_check = Psi(x_hat);
+    x_check = params.threshold(x_hat);
 
-    x_hat = x_hat + Phi_t((y - Phi(x_hat)));
-%     x_hat = x_hat + A_t(y - A(x_hat));
-    x_check = Psi(x_hat);
-    x_check = params.threshold(x_check);
-%     x_check = Psi(params.threshold(Psi_t(x_hat)));
-    x_bar = Psi_t(x_check);
-    x = x_bar + Phi_t ((y - Phi(x_bar)));
-%     x = x_check + A_t(y-A(x_check));
+%     x_bar = Psi_t(x_check);
+%     x = x_bar + Phi_t ((y - Phi(x_bar)));
+    x = x_check + A_t(y-A(x_check));
     D = RMS(x_hat, x);
     
   if ((D_prev ~= 0) && (abs(D - D_prev) < TOL))
@@ -53,15 +46,13 @@ x_hat = x_hat(:);
 
   D_prev = D;
 end
-
 params.end_levels = 1;
-x_hat = params.smoothing(x);
-x_hat = x_hat + Phi_t((y - Phi(x_hat)));
-x_check = Psi(x_hat);
-x_check = params.threshold(x_check);
-x_bar = Psi_t(x_check);
-x = x_bar + Phi_t ((y - Phi(x_bar)));
+x_hat = Psi(params.smoothing(Psi_t(x)));
+x_hat = x_hat + A_t(y - A(x_hat));
+x_check = params.threshold(x_hat,params.end_levels);
+x = x_check + A_t(y-A(x_check));
 
+x = Psi_t(x);
 reconstructed_image = reshape(x,params.imsize);
 
 
